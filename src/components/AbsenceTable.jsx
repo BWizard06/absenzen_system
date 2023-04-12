edad7859cc0599fcd72b6abbc3d2e41845f14fa4
BWizard06 <benbraendle@outlook.com>
2023-04-12 08:38:04 +0200

    list of students, and pop-up for each student

---
+++ src/components/AbsenceTable.jsx
@@ -0,0 +1,142 @@
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
+
+const AbsenceTable = ({ absences, addAbsence, deleteAbsence }) => {
+    const [newAbsence, setNewAbsence] = useState({
+        date: "",
+        lesson: "",
+        status: "",
+        reason: "",
+    });
+
+    const handleChange = (e) => {
+        setNewAbsence({ ...newAbsence, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (
+            newAbsence.date &&
+            newAbsence.lesson &&
+            newAbsence.status &&
+            newAbsence.reason
+        ) {
+            addAbsence(newAbsence);
+            setNewAbsence({ date: "", lesson: "", status: "", reason: "" });
+        } else {
+            alert("Bitte füllen Sie alle Felder aus.");
+        }
+    };
+
+    return (
+        <div>
+            <table className="table table-bordered">
+                <thead className="thead-dark">
+                    <tr>
+                        <th>Datum</th>
+                        <th>Lektion</th>
+                        <th>Status</th>
+                        <th>Grund</th>
+                        <th>Aktion</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {absences.map((absence, index) => (
+                        <tr key={index}>
+                            <td>{absence.date}</td>
+                            <td>{absence.lesson}</td>
+                            <td>{absence.status}</td>
+                            <td>{absence.reason}</td>
+                            <td>
+                                <button
+                                    className="btn btn-danger"
+                                    onClick={() => deleteAbsence(index)}
+                                >
+                                    Löschen
+                                </button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+
+            <h5 className="mt-4">Neue Absenz hinzufügen:</h5>
+            <form onSubmit={handleSubmit} className="mt-4">
+                <div className="row">
+                    <div className="col-md-6">
+                        <div className="form-group">
+                            <label>Datum:</label>
+                            <input
+                                type="date"
+                                name="date"
+                                value={newAbsence.date}
+                                onChange={handleChange}
+                                className="form-control"
+                            />
+                        </div>
+                    </div>
+                    <div className="col-md-6">
+                        <div className="form-group">
+                            <label>Lektion:</label>
+                            <input
+                                type="number"
+                                name="lesson"
+                                value={newAbsence.lesson}
+                                onChange={handleChange}
+                                className="form-control"
+                            />
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-6">
+                        <div className="form-group">
+                            <label>Status:</label>
+                            <select
+                                name="status"
+                                value={newAbsence.status}
+                                onChange={handleChange}
+                                className="form-control"
+                            >
+                                <option value="">--Status auswählen--</option>
+                                <option value="Abwesend">Abwesend</option>
+                                <option value="Entschuldigt">
+                                    Entschuldigt
+                                </option>
+                                <option value="Unentschuldigt">
+                                    Unentschuldigt
+                                </option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className="col-md-6">
+                        <div className="form-group">
+                            <label>Grund:</label>
+                            <input
+                                type="text"
+                                name="reason"
+                                value={newAbsence.reason}
+                                onChange={handleChange}
+                                className="form-control"
+                            />
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className="form-group">
+                            <Button
+                                type="submit "
+                                className="bg-primary text-white hover:bg-primary-dark"
+                            >
+                                Hinzufügen
+                            </Button>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default AbsenceTable;
---
+++ src/components/StudentTable.jsx
@@ -0,0 +1,206 @@
+import React, { useState } from "react";
+import { Modal, Button } from "react-bootstrap";
+import AbsenceTable from "./AbsenceTable";
+
+const students  = [
+    {
+        id: 1,
+        firstName: "Max",
+        lastName: "Mustermann",
+        class: "3A",
+        latestAbsence: "2023-04-01",
+        absenceCount: 2,
+        email: "max.mustermann@example.com",
+        absences: [
+            {
+                date: "2023-04-01",
+                lesson: 4,
+                status: "Abwesend",
+                reason: "Krank",
+            },
+            {
+                date: "2023-03-28",
+                lesson: 2,
+                status: "Entschuldigt",
+                reason: "Arzttermin",
+            },
+            // Fügen Sie hier weitere Absenz-Objekte hinzu
+        ],
+    },
+    {
+        id: 1,
+        firstName: "Max",
+        lastName: "Mustermann",
+        class: "3A",
+        latestAbsence: "2023-04-01",
+        absenceCount: 2,
+        email: "max.mustermann@example.com",
+        absences: [
+            {
+                date: "2023-04-01",
+                lesson: 4,
+                status: "Abwesend",
+                reason: "Krank",
+            },
+            {
+                date: "2023-03-28",
+                lesson: 2,
+                status: "Entschuldigt",
+                reason: "Arzttermin",
+            },
+            // Fügen Sie hier weitere Absenz-Objekte hinzu
+        ],
+    },
+    {
+        id: 1,
+        firstName: "Max",
+        lastName: "Mustermann",
+        class: "3A",
+        latestAbsence: "2023-04-01",
+        absenceCount: 2,
+        email: "max.mustermann@example.com",
+        absences: [
+            {
+                date: "2023-04-01",
+                lesson: 4,
+                status: "Abwesend",
+                reason: "Krank",
+            },
+            {
+                date: "2023-03-28",
+                lesson: 2,
+                status: "Entschuldigt",
+                reason: "Arzttermin",
+            },
+            // Fügen Sie hier weitere Absenz-Objekte hinzu
+        ],
+    },
+    {
+        id: 1,
+        firstName: "Max",
+        lastName: "Mustermann",
+        class: "3A",
+        latestAbsence: "2023-04-01",
+        absenceCount: 2,
+        email: "max.mustermann@example.com",
+        absences: [
+            {
+                date: "2023-04-01",
+                lesson: 4,
+                status: "Abwesend",
+                reason: "Krank",
+            },
+            {
+                date: "2023-03-28",
+                lesson: 2,
+                status: "Entschuldigt",
+                reason: "Arzttermin",
+            },
+            // Fügen Sie hier weitere Absenz-Objekte hinzu
+        ],
+    },
+    // Fügen Sie hier weitere Schüler-Objekte hinzu
+];
+
+
+const StudentTable = () => {
+    const [showModal, setShowModal] = useState(false);
+    const [selectedStudent, setSelectedStudent] = useState(null);
+
+    const handleClose = () => setShowModal(false);
+    const handleShow = (student) => {
+        const studentCopy = { ...student, absences: [...student.absences] };
+        setSelectedStudent(studentCopy);
+        setShowModal(true);
+    };
+      
+	  
+    const addAbsence = (newAbsence) => {
+        setSelectedStudent((prevStudent) => ({
+          ...prevStudent,
+          absences: [...prevStudent.absences, newAbsence],
+        }));
+    };
+      
+      const deleteAbsence = (indexToDelete) => {
+        setSelectedStudent((prevStudent) => ({
+          ...prevStudent,
+          absences: prevStudent.absences.filter(
+            (_, index) => index !== indexToDelete
+          ),
+        }));
+    };
+      
+    return (
+        <div className="container-fluid">
+            <table className="table table-bordered">
+                <thead className="thead-dark">
+                    <tr>
+                        <th scope="col">Profilbild</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Klasse</th>
+                        <th scope="col">Neuste Absenz</th>
+                        <th scope="col">Anzahl Absenzen</th>
+                    </tr>
+                </thead>
+                <tbody className="bg-white divide-y divide-gray-200">
+                    {students.map((student, index) => (
+                        <tr
+                            key={student.id}
+                            className={index % 2 === 1 ? "bg-gray-100" : ""}
+                        >
+                            <td></td>
+                            <td
+                                className="cursor-pointer text-blue-500 underline"
+                                onClick={() => handleShow(student)}
+                            >
+                                {student.firstName} {student.lastName}
+                            </td>
+                            <td>{student.class}</td>
+                            <td>{student.latestAbsence}</td>
+                            <td>{student.absenceCount}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+
+            <Modal show={showModal} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>
+                        {selectedStudent?.firstName} {selectedStudent?.lastName}
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <div className="flex flex-col">
+                        <div className="flex">
+                            <div
+                                className="bg-gray-300 w-20 h-20 mr-4"
+                                style={{ borderRadius: "25%" }}
+                            ></div>
+                            <div>
+                                <p>Klasse: {selectedStudent?.class}</p>
+                                <p>Email: {selectedStudent?.email}</p>
+                            </div>
+                        </div>
+                        <h5 className="mt-4">Absenzen:</h5>
+                        <AbsenceTable
+                            absences={selectedStudent?.absences}
+                            addAbsence={addAbsence}
+                            deleteAbsence={deleteAbsence}
+                        />
+                    </div>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button
+                        className="bg-primary text-white hover:bg-primary-dark"
+                        onClick={handleClose}
+                    >
+                        Schließen
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </div>
+    );
+};
+
+export default StudentTable;
